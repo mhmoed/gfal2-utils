ec7d9722bb023880420fa6d06e64fde85bc874b4
Matthijs Moed <m.h.moed@lumc.nl>
2015-05-12 17:35:07 +0200

    Added long format listing switch.

--- gfal-find.cpp
+++ gfal-find.cpp
@@ -3,12 +3,23 @@
 #include <stack>
 #include <memory>
 #include <algorithm>
+#include <ctime>
 
 #include <boost/program_options.hpp>
 
 #include <fnmatch.h>
 
 #include "gfal2.hpp"
+
+
+// Constants
+
+const int NUM_SECONDS_IN_SIX_MONTHS = 15552000;
+const int DATETIME_BUFFER_SIZE = 20;
+const int SIZE_FIELD_LENGTH = 12;
+
+const std::string OLD_FORMAT = "%h %e  %Y";
+const std::string NEW_FORMAT = "%h %e %R";
 
 
 // Display filters
@@ -54,7 +65,109 @@ struct shell_pattern_filter:public display_filter
 };
 
 
-void find(const std::string &root, const std::vector<std::unique_ptr<display_filter>> &display_filters)
+// Formatters
+
+struct formatter
+{
+    virtual std::string operator()(const std::string &path, const gfal2::directory_entry &entry) = 0;
+};
+
+
+struct short_formatter:public formatter
+{
+    virtual std::string operator()(const std::string &path, const gfal2::directory_entry &entry)
+    {
+        return path;
+    }
+};
+
+
+struct long_formatter:public formatter
+{
+    virtual std::string operator()(const std::string &path, const gfal2::directory_entry &entry)
+    {
+        std::string line;
+
+        // Permissions
+
+        if (gfal2::is_directory(entry))
+            line = "d";
+        else if (gfal2::is_symlink(entry))
+            line = "l";
+        else
+            line = "-";
+
+
+        auto mode = entry.status.st_mode;
+
+        // User
+
+        line += mode & S_IRUSR ? "r" : "-";
+        line += mode & S_IWUSR ? "w" : "-";
+        line += mode & S_IXUSR ? "x" : "-";
+
+        // Group
+
+        line += mode & S_IRGRP ? "r": "-";
+        line += mode & S_IWGRP ? "w": "-";
+        line += mode & S_IXGRP ? "x": "-";
+
+        // World
+
+        line += mode & S_IROTH ? "r": "-";
+        line += mode & S_IWOTH ? "w": "-";
+        line += mode & S_IXOTH ? "x": "-";
+
+        line += " ";
+
+        // Number of symbolic links
+
+        line += boost::lexical_cast<std::string>(entry.status.st_nlink);
+        line += "\t";
+
+        // Group ID
+
+        line += boost::lexical_cast<std::string>(entry.status.st_gid);
+        line += "\t";
+
+        // User ID
+
+        line += boost::lexical_cast<std::string>(entry.status.st_uid);
+        line += "\t";
+
+        // Modification time
+
+        auto current_time = std::time(nullptr);
+        auto &mod_time = entry.status.st_mtime;
+
+        char buffer[DATETIME_BUFFER_SIZE];
+
+        const std::string &datetime_format = (current_time - mod_time) > NUM_SECONDS_IN_SIX_MONTHS ? OLD_FORMAT : NEW_FORMAT;
+        
+        strftime(buffer, 20, datetime_format.c_str(), localtime(&mod_time));
+        
+        line += buffer;
+        line += "\t";
+
+        // Size
+
+        const std::string size_field = gfal2::is_directory(entry) ? "0" : boost::lexical_cast<std::string>(entry.status.st_size);
+        line += std::string(std::max<long>(SIZE_FIELD_LENGTH - size_field.length(), 0), ' ');  // Pad with leading empty spaces to align
+        line += size_field;
+        line += "\t";
+
+        // Path
+
+        line += path;
+
+        return std::string(line);
+    }
+};
+
+
+void find(const std::string &root,
+          const std::vector<std::unique_ptr<display_filter>> &display_filters,
+          const std::unique_ptr<formatter> &formatter)
 {
     using namespace std;
     using gfal2::DIRECTORY_SEPARATOR;
@@ -79,7 +192,7 @@ void find(const std::string &root, const std::vector<std::unique_ptr<display_fil
             const auto path = relative_path.empty() ? entry.name : relative_path + DIRECTORY_SEPARATOR + entry.name;
 
             if (none_of(display_filters.cbegin(), display_filters.cend(), [&entry](auto &filter){ return (*filter)(entry); }))
-                cout << path << endl;
+                cout << (*formatter)(path, entry) << endl;
 
             // Push to stack for further listing if directory
 
@@ -102,6 +215,7 @@ int main(const int argc, char **argv)
         ("help,h", "show help message")
         ("type", po::value<string>(), "type of file (f or d)")
         ("name", po::value<string>(), "match basename against shell pattern")
+        ("long,l", "show entries in long listing format")
         ("url", po::value<string>(), "URL to search from");
     
     po::positional_options_description p;
@@ -146,14 +260,16 @@ int main(const int argc, char **argv)
         }
 
         // Name filter
-        
+
         if (vm.count("name"))
         {
             const auto &pattern = vm["name"].as<string>();
             display_filters.push_back(move(unique_ptr<shell_pattern_filter>(new shell_pattern_filter(pattern))));
         }
 
-        find(vm["url"].as<string>(), display_filters);
+        const unique_ptr<formatter> format(move(unique_ptr<formatter>(vm.count("long") ? static_cast<formatter*>(new long_formatter) : static_cast<formatter*>(new short_formatter))));
+
+        find(vm["url"].as<string>(), display_filters, format);
     }
     catch (const std::exception &exception)
     {
